e06716d16c848e705ba8ceb9757e19f46ea0d46e
huojinx <2412320239@qq.com>
2023-02-19 11:08:13 +0800

    ggg

---
+++ src/helloworld2/Helloworld.java
@@ -0,0 +1,11 @@
+package helloworld2;
+
+public class Helloworld {
+
+	public Helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("ddd");
+	}
+}
