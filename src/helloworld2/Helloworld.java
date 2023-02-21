0dd89faff4fe0ac3de50f6f0f2a9146d4e945033
huojinx <2412320239@qq.com>
2023-02-21 15:02:38 +0800

    修改

--- src/helloworld2/Helloworld.java
+++ src/helloworld2/Helloworld.java
@@ -6,6 +6,8 @@ public class Helloworld {
 		// TODO Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
-		System.out.println("ddd");
+		System.out.println("Wow World!");
+		System.out.println(System.getProperty("java.version"));
+		System.out.println("子曰：“君子求诸己，小人求诸人。");
 	}
 }
